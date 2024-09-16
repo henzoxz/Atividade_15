3892ee7fe2cc5de3f2867045fbbc3e1e4532b6f1
henzoxz <henzomag2009@gmail.com>
2024-09-16 09:05:58 -0300

    atividade15.py

--- atividade15.py
+++ atividade15.py
@@ -4,7 +4,7 @@ numero2 = int(input(" Digite o 2º Número "))
 operacao = input(" Escolha a Operação desejada: Soma, Subtração, Multiplicação ou Divisão:  ")
 if(operacao=="Soma"):
     resultado = (numero1 + numero2)
-    print(f" Seu Resultado é:100 {resultado}")
+    print(f" Seu Resultado é: {resultado}")
 
 if(operacao=="Subtração"):
     resultado = (numero1 - numero2)
